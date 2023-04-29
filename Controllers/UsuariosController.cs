5ca2512c1d43a4060e1ab70692470c7926cf9741
francinjr <francinaldomanoel135@gmail.com>
2023-04-29 09:37:16 -0300

    atividade 5

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,67 @@
+using System;
+using Exo.WebApi.Models;
+using Exo.WebApi.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Exo.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly UsuarioRepository _usuarioRepository;
+
+        public UsuariosController(UsuarioRepository usuarioRepository)
+        {
+            _usuarioRepository = usuarioRepository;
+        }
+
+        // /api/usuarios
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            return Ok(_usuarioRepository.Listar());
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Usuario usuario)
+        {
+            _usuarioRepository.Cadastrar(usuario);
+            return StatusCode(201);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult BuscaPorId(int id)
+        {
+            Usuario usuario = _usuarioRepository.BuscaPorId(id);
+            if(usuario == null)
+            {
+                return Ok(usuario);
+            }
+            return Ok(usuario);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Atualizar(int id, Usuario usuario)
+        {
+            _usuarioRepository.Atualizar(id, usuario);
+            return StatusCode(204);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Deletar(int id)
+        {
+            try
+            {
+                _usuarioRepository.Deletar(id);
+                return StatusCode(204);
+            }
+            catch (Exception e)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
